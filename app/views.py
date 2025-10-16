a1f90484a24674a49bcdf6a039348fee112e58d8
KohakuC <60190850+KohakuC@users.noreply.github.com>
2025-10-16 15:15:13 +0200

    Modifications sur views.py

--- app/views.py
+++ app/views.py
@@ -1,5 +1,9 @@
+
+import os
 from django.http import HttpResponse
 from django.shortcuts import render
+from app import settings
+os.listdir(settings.BASE_DIR / 'data')
 
 def index(request):
     context = {"name" : "Laetitia", "breadcrumb" : [('Home', '/')]}
